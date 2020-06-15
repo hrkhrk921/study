e73253cc89429a27d93287b9bd1133c4bad568df
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-06-16 00:35:47 +0900

    view修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = Users.all
   end
+
   def show
      @user = User.find(params[:id])
+     @book = Book.new
   end
+
   def edit
     @user = current_user
   end
+
+  def update
+    
+  end
+
   def destroy
     
   end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_095619) do
+ActiveRecord::Schema.define(version: 2020_06_15_145145) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
+    t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "title"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
